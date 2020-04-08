704ddb57cc658ba3016fdf2c327cfed20469c5b5
yulia-kh <y.khisamutdinova@gmail.com>
2020-04-07 22:31:31 -0700

    Add version function

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,40 @@
 import minimist from 'minimist';
+// import { search } from './search';
+// import { list } from './list';
+// import { help } from './help';
+import { version } from './version';
 
-export async function cli(args) {
-    const argsObj = minimist(args.slice(2));
-    console.log(argsObj);
+export async function cli(argsArray) {
+  const args = minimist(argsArray.slice(2));
+  let cmd = args._[0] || 'help';
+
+  if (args.version || args.v) {
+    cmd = 'version';
+  }
+
+  // if (args.help || args.h) {
+  //   cmd = 'help';
+  // }
+
+  switch (cmd) {
+    case 'version':
+      version(args);
+      break;
+
+    case 'help':
+      help(args);
+      break;
+
+    case 'search':
+      search(args);
+      break;
+
+    case 'list':
+      list(args);
+      break;
+    
+    default:
+      console.error(`"${cmd}" is not a valid command!`);
+      break;
+  }
 }
---
+++ src/version.js
@@ -0,0 +1,4 @@
+export async function version() {
+    const packageJson = require('../package.json');
+    console.log(packageJson.version);
+}
